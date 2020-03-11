88f2a307edf428cdec35bea650ff1d6ec1cec3b6
Charles VUILLECARD <charles.vuillec@gmail.com>
2020-03-11 14:13:45 +0100

    [terminal] - adding dcall.bat for windows

---
+++ core/app/client/terminal/src/main/resources/dcall.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set PWD=%~dp0
+set JAVA_BIN=%JAVA_HOME%\bin\java.exe
+
+echo _____________________________________________________________
+echo Working directory : %PWD%
+echo Using : %JAVA_BIN%
+echo *************************************************************
+echo * Starting DCall in silent mode (detashed from current tty) *
+echo *      > Trying to connect to [ %* ]                        *
+echo *************************************************************
+
+"%JAVA_BIN% -jar ${project.artifactId}-${project.version}.${project.packaging} %*"
