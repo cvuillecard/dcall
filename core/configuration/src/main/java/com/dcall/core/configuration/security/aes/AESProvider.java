dc9dfcc9be622e7cac98cb3a2def81f4670c04be
Charles VUILLECARD <charles.vuillec@gmail.com>
2020-03-14 14:04:14 +0100

    [AESProvider] - add method decryptFileBytes() which reads all bytes of a file using cipher AES decryption + one test

--- core/configuration/src/main/java/com/dcall/core/configuration/security/aes/AESProvider.java
+++ core/configuration/src/main/java/com/dcall/core/configuration/security/aes/AESProvider.java
@@ -4,23 +4,39 @@ import com.dcall.core.configuration.security.rsa.RSAProvider;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.nio.channels.Channels;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Base64;
 
 public final class AESProvider {
+    private static final int MAX_BUFFER_SIZE = Integer.MAX_VALUE - 8;
     private static final String DEFAULT_CHARSET = "UTF-8";
     private static final int _KEY_LENGTH = 128; // 256 needs JCE
     private static final int _BUFFER_SIZE = 8192;
+    private static final int _ITERATE_SIZE = 10000;
 
     public static SecretKey generateSecretKey() throws Exception {
         KeyGenerator gen = KeyGenerator.getInstance("AES");
         gen.init(_KEY_LENGTH);
 
         return gen.generateKey();
+    }
+
+    public static SecretKey getSecretKey(final String password, final byte[] salt) throws Exception {
+        final SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        final SecretKey secretKey = factory.generateSecret(new PBEKeySpec(password.toCharArray(), salt, _ITERATE_SIZE, _KEY_LENGTH));
+
+        return new SecretKeySpec(secretKey.getEncoded(), "AES");
     }
 
     public static IvParameterSpec generateIV() {
@@ -30,10 +46,18 @@ public final class AESProvider {
         return new IvParameterSpec(iv);
     }
 
-    public static Cipher initCipher(final int cipherEncryptMode, final SecretKey secretKey, final IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
+    public static Cipher initCipherWithIV(final int cipherEncryptMode, final SecretKey secretKey, final IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
 
         final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cipher.init(cipherEncryptMode, secretKey, iv);
+
+        return cipher;
+    }
+
+    public static Cipher initCipher(final int cipherEncryptMode, final SecretKey secretKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+
+        final Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(cipherEncryptMode, secretKey);
 
         return cipher;
     }
@@ -102,5 +126,55 @@ public final class AESProvider {
 
         cin.close();
         cout.close();
+    }
+
+    /**
+     * Decrypt all bytes of a file using cipher AES
+     *
+     * @param filePath
+     * @param cipher
+     * @return
+     * @throws IOException
+     */
+    public static byte[] decryptFileBytes(final String filePath, final Cipher cipher) throws IOException {
+        try (SeekableByteChannel sbc = Files.newByteChannel(Paths.get(filePath));
+             final InputStream in = Channels.newInputStream(sbc)) {
+            final long size = sbc.size();
+            if (size > (long)MAX_BUFFER_SIZE)
+                throw new OutOfMemoryError("Required array size too large");
+
+            final CipherInputStream cin = new CipherInputStream(in, cipher);
+            return read(cin, (int)size);
+        }
+    }
+
+    private static byte[] read(InputStream cin, int initialSize) throws IOException {
+        int capacity = initialSize;
+        byte[] buf = new byte[capacity];
+        int nread = 0;
+        int n;
+        for (;;) {
+            // read to EOF which may read more or less than initialSize (eg: file
+            // is truncated while we are reading)
+            while ((n = cin.read(buf, nread, capacity - nread)) > 0)
+                nread += n;
+
+            // if last call to source.read() returned -1, we are done
+            // otherwise, try to read one more byte; if that failed we're done too
+            if (n < 0 || (n = cin.read()) < 0)
+                break;
+
+            // one more byte was read; need to allocate a larger buffer
+            if (capacity <= MAX_BUFFER_SIZE - capacity) {
+                capacity = Math.max(capacity << 1, _BUFFER_SIZE);
+            } else {
+                if (capacity == MAX_BUFFER_SIZE)
+                    throw new OutOfMemoryError("Required array size too large");
+                capacity = MAX_BUFFER_SIZE;
+            }
+            buf = Arrays.copyOf(buf, capacity);
+            buf[nread++] = (byte)n;
+        }
+        return (capacity == nread) ? buf : Arrays.copyOf(buf, nread);
     }
 }
--- core/configuration/src/test/java/com/dcall/core/configuration/security/aes/AESProviderTest.java
+++ core/configuration/src/test/java/com/dcall/core/configuration/security/aes/AESProviderTest.java
@@ -8,12 +8,17 @@ import org.junit.Test;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Base64;
 
 public class AESProviderTest {
     private static SecretKey _secret;
     private static IvParameterSpec _iv;
+    private static final String password = "q7HyVPwdjVp0ifD9++mbug==";
+    private static final byte[] salt = "supersalt%#!@$#|-*+".getBytes();
 
     @BeforeClass
     public static void init() throws Exception {
@@ -30,13 +35,47 @@ public class AESProviderTest {
     }
 
     @Test
-    public void should_encrypt_string_encryptString() throws Exception {
+    public void should_generate_secret_key_with_password_and_salt_getSecretKey() throws Exception {
+        final String secret = Base64.getEncoder().encodeToString(AESProvider.getSecretKey(password, salt).getEncoded());
+        final String secret2 = Base64.getEncoder().encodeToString(AESProvider.getSecretKey(password, salt).getEncoded());
+
+        Assert.assertEquals(secret, secret2);
+    }
+
+    @Test
+    public void should_encrypt_string_with_IV_encryptString() throws Exception {
         final String msg = "message test";
-        final Cipher enc = AESProvider.initCipher(Cipher.ENCRYPT_MODE, _secret, _iv);
-        final Cipher dec = AESProvider.initCipher(Cipher.DECRYPT_MODE, _secret, _iv);
+        final Cipher enc = AESProvider.initCipherWithIV(Cipher.ENCRYPT_MODE, _secret, _iv);
+        final Cipher dec = AESProvider.initCipherWithIV(Cipher.DECRYPT_MODE, _secret, _iv);
 
         final byte[] cryptedMsg = AESProvider.encryptString(msg, enc);
         final byte[] decryptedMsg = AESProvider.decryptString(cryptedMsg, dec);
+
+        Assert.assertNotEquals(msg, new String(cryptedMsg));
+        Assert.assertEquals(msg, new String(decryptedMsg));
+    }
+
+    @Test
+    public void should_encrypt_string_without_IV_encryptString() throws Exception {
+        String msg = "message test";
+        String secret = Base64.getEncoder().encodeToString(_secret.getEncoded());
+
+        // using SecretKey
+        Cipher enc = AESProvider.initCipher(Cipher.ENCRYPT_MODE, _secret);
+        Cipher dec = AESProvider.initCipher(Cipher.DECRYPT_MODE, _secret);
+
+        byte[] cryptedMsg = AESProvider.encryptString(msg, enc);
+        byte[] decryptedMsg = AESProvider.decryptString(cryptedMsg, dec);
+
+        Assert.assertNotEquals(msg, new String(cryptedMsg));
+        Assert.assertEquals(msg, new String(decryptedMsg));
+
+        // using SecretKeySpec
+        enc = AESProvider.initCipher(Cipher.ENCRYPT_MODE, new SecretKeySpec(_secret.getEncoded(), "AES"));
+        dec = AESProvider.initCipher(Cipher.DECRYPT_MODE, new SecretKeySpec(_secret.getEncoded(), "AES"));
+
+        cryptedMsg = AESProvider.encryptString(msg, enc);
+        decryptedMsg = AESProvider.decryptString(cryptedMsg, dec);
 
         Assert.assertNotEquals(msg, new String(cryptedMsg));
         Assert.assertEquals(msg, new String(decryptedMsg));
@@ -47,8 +86,8 @@ public class AESProviderTest {
         final String inputFile = "./src/test/resources/cert.pem";
         final String encryptedFile = "./src/test/resources/cert-encrypted.pem";
         final String decryptedFile = "./src/test/resources/cert-decrypted.pem";
-        final Cipher enc = AESProvider.initCipher(Cipher.ENCRYPT_MODE, _secret, _iv);
-        final Cipher dec = AESProvider.initCipher(Cipher.DECRYPT_MODE, _secret, _iv);
+        final Cipher enc = AESProvider.initCipherWithIV(Cipher.ENCRYPT_MODE, _secret, _iv);
+        final Cipher dec = AESProvider.initCipherWithIV(Cipher.DECRYPT_MODE, _secret, _iv);
 
         AESProvider.encryptFile(inputFile, encryptedFile, enc);
         AESProvider.decryptFile(encryptedFile, decryptedFile, dec);
@@ -67,8 +106,8 @@ public class AESProviderTest {
         final String encryptedFile = "./src/test/resources/image-encrypted.png";
         final String decryptedFile = "./src/test/resources/image-decrypted.png";
 
-        final Cipher enc = AESProvider.initCipher(Cipher.ENCRYPT_MODE, _secret, _iv);
-        final Cipher dec = AESProvider.initCipher(Cipher.DECRYPT_MODE, _secret, _iv);
+        final Cipher enc = AESProvider.initCipherWithIV(Cipher.ENCRYPT_MODE, _secret, _iv);
+        final Cipher dec = AESProvider.initCipherWithIV(Cipher.DECRYPT_MODE, _secret, _iv);
 
         AESProvider.encryptFile(inputFile, encryptedFile, enc);
         AESProvider.decryptFile(encryptedFile, decryptedFile, dec);
@@ -79,6 +118,26 @@ public class AESProviderTest {
 
         Assert.assertNotEquals(md5sumInputFile, md5sumEncFile);
         Assert.assertEquals(md5sumInputFile, md5sumDecFile);
+    }
+
+    @Test
+    public void sould_decrypt_all_bytes_of_encrypted_file() throws Exception {
+        final String inputFile = "./src/test/resources/cert.pem";
+        final String encryptedFile = "./src/test/resources/cert-encrypted.pem";
+        final String decryptedFile = "./src/test/resources/cert-decrypted.pem";
+        final Cipher inCipher = AESProvider.initCipher(Cipher.ENCRYPT_MODE, AESProvider.getSecretKey(password, salt));
+        final Cipher outCipher = AESProvider.initCipher(Cipher.DECRYPT_MODE, AESProvider.getSecretKey(password, salt));
+
+        AESProvider.encryptFile(inputFile, encryptedFile, inCipher);
+        AESProvider.decryptFile(encryptedFile, decryptedFile, outCipher);
+
+        // when : we read all bytes of the decrypted file and the same file encrypted with our method
+        final byte[] allBytes = Files.readAllBytes(Paths.get(decryptedFile));
+        final byte[] decryptedAllBytes = AESProvider.decryptFileBytes(encryptedFile, outCipher);
+
+        // then : the buffers must be equals, means we rode the file decrypting all encrypted bytes
+        Assert.assertEquals(new String(allBytes), new String(decryptedAllBytes));
+        Assert.assertEquals(Base64.getEncoder().encodeToString(allBytes), Base64.getEncoder().encodeToString(decryptedAllBytes));
     }
 
 }
