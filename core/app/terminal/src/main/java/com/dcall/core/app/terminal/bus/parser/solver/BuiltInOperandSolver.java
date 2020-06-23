1878b5c539c7ff84e82840779cc7da724371e891
Charles VUILLECARD <charles.vuillec@gmail.com>
2020-06-23 15:06:36 +0200

    [BuiltInOperandSolver] - adding getCmdArgs()

--- core/app/terminal/src/main/java/com/dcall/core/app/terminal/bus/parser/solver/BuiltInOperandSolver.java
+++ core/app/terminal/src/main/java/com/dcall/core/app/terminal/bus/parser/solver/BuiltInOperandSolver.java
@@ -6,8 +6,14 @@ import com.dcall.core.configuration.generic.parser.IterStringUtils;
 import com.dcall.core.configuration.generic.parser.expression.operand.solver.OperandSolver;
 import com.dcall.core.configuration.generic.parser.expression.operand.solver.StringOperandSolver;
 import com.dcall.core.configuration.utils.HelpUtils;
+import com.dcall.core.configuration.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class BuiltInOperandSolver implements OperandSolver {
     private static final Logger LOG = LoggerFactory.getLogger(BuiltInOperandSolver.class);
@@ -21,7 +27,7 @@ public final class BuiltInOperandSolver implements OperandSolver {
 
             if (cmd != null) {
                 final int nextArgIdx = IterStringUtils.iterFront(seq, endIdx, c -> ASCII.isBlank(c));
-                final String[] args = nextArgIdx > endIdx ? seq.subSequence(nextArgIdx, seq.length()).toString().split(" ") : null;
+                final String[] args = nextArgIdx > endIdx ? getCmdArgs(seq.subSequence(nextArgIdx, seq.length())) : null;
                 return (T) cmd.getService().setHelp(HelpUtils.getBuiltInHelp(cmdName)).setParams(args);
             }
         }
@@ -30,5 +36,23 @@ public final class BuiltInOperandSolver implements OperandSolver {
         }
 
         return new StringOperandSolver().solve(seq);
+    }
+
+    public String[] getCmdArgs(final CharSequence seq) {
+        final List<String> cmdArgs = new ArrayList<>();
+        final int endOptionIdx = IterStringUtils.iterFront(seq, 0, c -> !ASCII.isSpace(c));
+        final String option = seq.subSequence(0, endOptionIdx).toString();
+        final Pattern pattern = Pattern.compile("([\\w-]+)=((.*?)([\\w-]+)(.*?)\\3)");
+        final Matcher matcher = pattern.matcher(seq.toString());
+
+        while (matcher.find()) {
+            final String keyValue = matcher.group(1) + '=' + matcher.group(2);
+            cmdArgs.add(keyValue);
+            LOG.debug(keyValue);
+        }
+        if(!cmdArgs.isEmpty())
+            cmdArgs.add(0, option);
+
+        return cmdArgs.isEmpty() ? StringUtils.trim(seq).toString().split(" ") : cmdArgs.toArray(new String[cmdArgs.size()]);
     }
 }
