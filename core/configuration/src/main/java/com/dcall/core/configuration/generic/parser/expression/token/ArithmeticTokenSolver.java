bdb86bcce57da227e9afb0aa438273d8b817ffa4
Charles VUILLECARD <charles.vuillec@gmail.com>
2020-06-24 12:01:34 +0200

    [TokenSolver] - adding methods for futur extension behaviour of condition methods

--- core/configuration/src/main/java/com/dcall/core/configuration/generic/parser/expression/token/ArithmeticTokenSolver.java
+++ core/configuration/src/main/java/com/dcall/core/configuration/generic/parser/expression/token/ArithmeticTokenSolver.java
@@ -6,29 +6,34 @@ import java.util.function.Predicate;
 
 public class ArithmeticTokenSolver implements TokenSolver {
     @Override
-    public Predicate<Character> isBlank() {
-        return c -> ASCII.isBlank(c);
-    }
+    public Predicate<Character> isBlank() { return c -> ASCII.isBlank(c); }
 
     @Override
-    public Predicate<Character> isNotToken() {
-        return c -> !ASCII.isOperator(c) && !ASCII.isParenthesis(c);
-    }
+    public boolean isBlank(final CharSequence seq, final int idx) {  return isBlank().test(seq.charAt(idx)); }
 
     @Override
-    public Predicate<Character> isOpenToken() {
-        return c -> ASCII.isOpenParenthesis(c);
-    }
+    public Predicate<Character> isNotToken() { return c -> !ASCII.isOperator(c) && !ASCII.isParenthesis(c); }
 
     @Override
-    public Predicate<Character> isCloseToken() {
-        return c -> ASCII.isCloseParenthesis(c);
-    }
+    public boolean isNotToken(final CharSequence seq, final int idx) { return isNotToken().test(seq.charAt(idx)); }
 
     @Override
-    public Predicate<Character> isOperator() {
-        return c -> ASCII.isOperator(c);
-    }
+    public Predicate<Character> isOpenToken() { return c -> ASCII.isOpenParenthesis(c); }
+
+    @Override
+    public boolean isOpenToken(final CharSequence seq, final int idx) { return isOpenToken().test(seq.charAt(idx)); }
+
+    @Override
+    public Predicate<Character> isCloseToken() { return c -> ASCII.isCloseParenthesis(c); }
+
+    @Override
+    public boolean isCloseToken(final CharSequence seq, final int idx) { return isCloseToken().test(seq.charAt(idx)); }
+
+    @Override
+    public Predicate<Character> isOperator() { return c -> ASCII.isOperator(c); }
+
+    @Override
+    public boolean isOperator(final CharSequence seq, final int idx) { return isOperator().test(seq.charAt(idx)); }
 
     @Override
     public int iterTokenGroup(final CharSequence seq, int idx, final int endIdx) {
